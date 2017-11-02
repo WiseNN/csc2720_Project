2b556b41918b6a6024ca18b6e404d034cef3bb61
WiseNN <wise.norris1@gmail.com>
2017-11-02 17:08:19 -0400

    updated API routes, and included testing route

---
+++ src/api/routes.js
@@ -0,0 +1,131 @@
+import express from 'express';
+import myConst from '../constants';
+// var express = require('express');
+
+var router = express.Router();
+
+// router.get("/convo/:convoName", function(req,res){
+// 	console.log("JSON.stringify: "+JSON.stringify(req.params));
+
+// });
+
+//used to validate user exists
+/*
+	{
+		UID : String [userId]
+	}
+*/
+router.get("/user/:userId", (req,res) => {
+	console.log("params: "+JSON.stringify(req.params));
+	res.send({
+
+		users : req.params.userId,
+		exists: true
+	});
+});
+
+
+
+//private chat messages between user/responder/messages (messages will be sorted by uid key created by mongoDB)
+/*
+{
+	privateChat :{
+			userId :{
+				recipientId :{
+						messages :{
+							date : `_DATE`,
+							time : _TIME,
+							sender : _recipientId or _userId,
+							messages : _MESSAGE
+								
+					}
+				}
+	
+		} 
+	}
+}
+*/
+
+router.get("/privateChat/:userId/:recipient/messages", function(req,res){
+	
+	const dateTime = getDateAndTime();
+
+	const message = {
+		date : dateTime.date,
+		time : dateTime.time
+	};
+	res.send(message);
+
+});
+
+//function will be moved to a Class called Utilities folder
+const getDateAndTime = () => {
+
+	//create date object
+	const date = new Date();
+
+	//get month, day & year to create a formatted Date string
+	var month = "";
+	const day = date.getDay();
+	const year = date.getYear();
+
+	//get hour, min. & sec. to create formateed Time string
+	const hour = date.getHours();
+	const minutes = date.getMinutes();
+	const seconds = date.getSeconds();
+
+	//mapping month number to correct name
+	switch(date.getMonth())
+	{
+		case 1 : month = myConst.month.jan; break
+		case 2 : month = myConst.month.feb; break
+		case 3 : month = myConst.month.mar; break
+		case 4 : month = myConst.month.apr; break
+		case 5 : month = myConst.month.may; break
+		case 6 : month = myConst.month.jun; break
+		case 7 : month = myConst.month.jul; break
+		case 8 : month = myConst.month.aug; break
+		case 9 : month = myConst.month.sep; break
+		case 10 : month = myConst.month.oct; break
+		case 11 : month = myConst.month.nov; break
+		case 12 : month = myConst.month.dec; break
+		default : throw new Error("Not a valid month, please check: getDateAndTime() function");
+	}
+
+	//formatted Date & Time String ***DO NOT MODIFY***
+	const formattedDate_Str = month+" "+day+", "+year;
+	const formattedTime_Str = hour+":"+minutes+":"+seconds;
+	
+	
+
+	//returning a a JSON object with date & time string DO NOT MODIFY
+	return { date: formattedDate_Str, time: formattedTime_Str};
+
+};
+
+
+
+
+
+
+//used to retrieve list of users conversations 
+/*
+
+	userId :{
+		date :{
+	
+		}
+	}
+}
+*/
+router.get("/userPosts/:userId/:date/:uid/:message", (req,res) => {
+	console.log("JSON.stringify: "+JSON.stringify(req.params));
+	
+	res.send("HELLO Renai Lilly! [;)");	
+});
+
+
+
+
+
+export default router;
--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
-var express = require('express');
 
-var app = express();
+// const express = require('express');
+import express from 'express';
+import routes from './api/routes';
+
+const app = express();
 
 const port = 3300;
+
+
+
+app.get('/', function(req,res){
+	res.send("landing");
+});
+// router.get('/home', function(){});
+
+app.use('/api', routes);
+
+// app.get('*', function(req,res){
+
+// 	console.log("UP AND RUNNING!");
+// 	res.send("PAGE LOADEDD...");
+// });
+
+
 app.listen(port, function(){ console.log("listening on port: "+port);	
 } );
 
-app.get('*', function(req,res){
 
-	console.log("UP AND RUNNING!");
-	res.send("LOADEDD...");
-});
+
 
 
 
